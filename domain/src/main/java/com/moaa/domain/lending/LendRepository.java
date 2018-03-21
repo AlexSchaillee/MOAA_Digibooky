bcd1d331c2f39cbc984ed1186b210e57063cfa99
MRedant <mredant@gmail.com>
2018-03-21 15:07:15 +0100

    story 11 finished without tests

--- domain/src/main/java/com/moaa/domain/lending/LendRepository.java
+++ domain/src/main/java/com/moaa/domain/lending/LendRepository.java
@@ -1,18 +1,36 @@
 package com.moaa.domain.lending;
 
+import com.moaa.domain.lending.databases.LendDatabase;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 public class LendRepository {
 
-    private LendRepository lendRepository;
+    private LendDatabase lendDataBase;
 
     @Inject
-    public LendRepository(LendRepository lendRepository) {
-        this.lendRepository = lendRepository;
+    public LendRepository(LendDatabase lendDataBase) {
+        this.lendDataBase = lendDataBase;
     }
 
-//    public LendContract
+    public LendContract addLendContract(LendContract lendContract)throws IllegalArgumentException{
+        if (databaseContainsLendContract(lendContract)){
+            throw new IllegalArgumentException("Database already contains contract with id: " +lendContract.getLendId());
+        }
+        lendDataBase.addLendContract(lendContract);
+        return lendContract;
+    }
+
+    private boolean databaseContainsLendContract (LendContract lendContract){
+        boolean contains = false;
+        for (LendContract item: lendDataBase.getLendContractList()) {
+            if (item.getLendId().equals(lendContract.getLendId())){
+                contains = true;
+            }
+        }
+        return contains;
+    }
 
 }
--- service/src/main/java/com/moaa/service/lending/LendService.java
+++ service/src/main/java/com/moaa/service/lending/LendService.java
@@ -1,4 +1,36 @@
 package com.moaa.service.lending;
 
+import com.moaa.domain.lending.LendContract;
+import com.moaa.domain.lending.LendRepository;
+import com.moaa.service.books.BookService;
+import com.moaa.service.member.MemberService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
 public class LendService {
+
+    private final LendRepository lendRepository;
+    private final BookService bookService;
+    private final MemberService memberService;
+
+    @Inject
+    public LendService(LendRepository lendRepository, BookService bookService, MemberService memberService) {
+        this.lendRepository = lendRepository;
+        this.bookService = bookService;
+        this.memberService = memberService;
+    }
+
+    public LendContract addLendContract(String memberId, String bookIsbn ) throws IllegalArgumentException {
+        if (bookService.searchBookByIsbnPart(bookIsbn).size()>1){
+            throw new IllegalArgumentException("Please provide a unique Isbn. More than one book found for isbn-part: "+bookIsbn);
+        }
+        return lendRepository.addLendContract(
+                LendContract.LendContractBuilder.lendContract()
+                .withBook(bookService.searchBookByIsbnPart(bookIsbn).get(0))
+                .withMember(memberService.getMemberById(memberId))
+                .build());
+
+    }
 }
